c8cf72113d5643e9d6bfe649b6ffd5a1da378124
vigkrishna <kvig1_be22@thapar.edu>
2023-07-29 13:47:06 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req,res)=>{
     res.send("api is running")
 })
 
-app.use('https://intouchbackendkv.onrender.com/', userRoutes)
+app.use('https://intouchbackendkv.onrender.com', userRoutes)
 
 app.get("/api/chats", (req,res)=>{
     res.send(chats);
--- frontend/src/Context/chat.js
+++ frontend/src/Context/chat.js
@@ -6,6 +6,9 @@ function chat() {
     <h1>
         chatting ui 
     </h1>
+    <h2>
+        i will be working on it after few days
+    </h2>
     </>
   )
 }
