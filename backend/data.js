04d94943417622672290a13a93ccbba822f18826
vigkrishna <kvig1_be22@thapar.edu>
2023-08-01 22:24:07 +0530

    updated data.js

--- backend/data.js
+++ backend/data.js
@@ -21,6 +21,49 @@ users: [
 },
 
 {
+    isGroupChat: false,
+    
+    users: [
+        {
+         name : "user3",
+         email : "user3@gmail.com",
+        },
+    
+        {
+            name: "user4",
+            email: "user4@gmail.com",
+        }
+        
+    
+        ],
+        _id: "3",
+        chatName: "friends1",
+    
+    },
+
+
+    {
+        isGroupChat: true,
+        
+        users: [
+            {
+             name : "user5",
+             email : "user5@gmail.com",
+            },
+        
+            {
+                name: "user6",
+                email: "user6@gmail.com",
+            }
+            
+        
+            ],
+            _id: "4",
+            chatName: "friends2",
+        
+        },
+
+{
 
     isGroupChat: true,
 
