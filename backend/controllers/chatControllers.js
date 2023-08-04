bd358e12a44db652dd7e29f3a34252335422dc48
vigkrishna <kvig1_be22@thapar.edu>
2023-08-04 23:49:20 +0530

    ..

---
+++ backend/controllers/chatControllers.js
@@ -0,0 +1,30 @@
+const asyncHandler = require("express-async-handler");
+const Chat = require("../models/chatModel");
+const User = require("../models/userModel");
+
+//@description     Create or fetch One to One Chat
+//@route           POST /api/chat/
+//@access          Protected
+const accessChat = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    console.log("UserId param not sent with request");
+    return res.sendStatus(400);
+  }
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+})
