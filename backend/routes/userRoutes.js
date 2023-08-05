65bc2e014306793716942361585745a42d58cc90
vigkrishna <kvig1_be22@thapar.edu>
2023-08-05 14:01:28 +0530

    token functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const {registerUser, authUser, allUsers, chatpersons, welcomePage} = require("../controllers/userControllers")
 const router = express.Router();
+const {protect} = require("../middleware/authMiddleware")
 
-router.route("/register").post(registerUser).get(allUsers)
+
+
+router.route("/register").post(registerUser).get(protect,allUsers)
 router.post("/login", authUser).get(welcomePage)
 router.route("/chatbox").get(chatpersons)
 module.exports = router;
