288309ff69699eddc6a502625fbe91b8f99ebcca
vigkrishna <kvig1_be22@thapar.edu>
2023-08-10 22:10:23 +0530

    ..

--- frontend/src/components/miscellaneous/ChatBox.js
+++ frontend/src/components/miscellaneous/ChatBox.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import { Box } from "@chakra-ui/layout";
 function ChatBox() {
   return (
-    <div>ChatBox- 
-        here ui of all chats will be shown
-    </div>
-  )
+    <Box
+    d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+    alignItems="center"
+    flexDir="column"
+    p={3}
+    bg="white"
+    w={{ base: "100%", md: "68%" }}
+    borderRadius="lg"
+    borderWidth="1px"
+  >
+    <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+  </Box>
+);
+
+
+    
+  
 }
 
 export default ChatBox
