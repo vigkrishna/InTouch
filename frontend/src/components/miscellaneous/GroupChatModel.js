9e402307767b0f1b6b5ebe8187e2df7793773a05
vigkrishna <kvig1_be22@thapar.edu>
2023-08-17 22:46:37 +0530

    chat model updated

--- frontend/src/components/miscellaneous/GroupChatModel.js
+++ frontend/src/components/miscellaneous/GroupChatModel.js
@@ -20,3 +20,55 @@ import { useState } from "react";
 const handleDelete = (delUser) => {
     setSelectedUsers(selectedUsers.filter((sel) => sel._id !== delUser._id));
   };
+
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleRename = async () => {
+    if (!groupChatName) return;
+
+    try {
+      setRenameLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }
+      const handleAddUser = async (user1) => {
+        if (selectedChat.users.find((u) => u._id === user1._id)) {
+          toast({
+            title: "User Already in group!",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+          return;
+        }
