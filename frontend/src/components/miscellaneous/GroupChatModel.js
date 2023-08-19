1d84ba5691ff2c5bb8e4222d346a9ca9ec60d826
vigkrishna <kvig1_be22@thapar.edu>
2023-08-19 10:55:35 +0530

    ..

--- frontend/src/components/miscellaneous/GroupChatModel.js
+++ frontend/src/components/miscellaneous/GroupChatModel.js
@@ -17,6 +17,15 @@ import {
 
   import axios from "axios";
 import { useState } from "react";
+import { ChatState } from "../../Context/ChatProvider";
+import UserBadgeItem from "../userAvatar/UserBadgeItem";
+import UserListItem from "../userAvatar/UserListItem";
+
+
+const GroupChatModel =({
+  fetchMessages, fetchAgain, setFetchAgain
+})
+
 
 //to delete any user 
 const handleDelete = (delUser) => {
@@ -66,7 +75,7 @@ const handleDelete = (delUser) => {
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
-      }
+      }}}
     
       //to handle user in 
       const handleAddUser = async (user1) => {
@@ -80,7 +89,7 @@ const handleDelete = (delUser) => {
           });
           return;
         }
-
+      }
         //to remove a user 
         const handleRemove = async (user1) => {
           if (selectedChat.groupAdmin._id !== user._id && user1._id !== user._id) {
@@ -92,7 +101,7 @@ const handleDelete = (delUser) => {
               position: "bottom",
             });
             return;
-          }
+          }}
       
           try {
             setLoading(true);
@@ -101,3 +110,82 @@ const handleDelete = (delUser) => {
                 Authorization: `Bearer ${user.token}`,
               },
             }
+
+
+             return (
+    <>
+      <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="Work sans"
+            d="flex"
+            justifyContent="center"
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
+
+          <ModalCloseButton />
+          <ModalBody d="flex" flexDir="column" alignItems="center">
+            <Box w="100%" d="flex" flexWrap="wrap" pb={3}>
+              {selectedChat.users.map((u) => (
+                <UserBadgeItem
+                  key={u._id}
+                  user={u}
+                  admin={selectedChat.groupAdmin}
+                  handleFunction={() => handleRemove(u)}
+                />
+              ))}
+            </Box>
+            <FormControl d="flex">
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                value={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                variant="solid"
+                colorScheme="teal"
+                ml={1}
+                isLoading={renameloading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder="Add User to group"
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+
+            {loading ? (
+              <Spinner size="lg" />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => handleAddUser(user)}
+                />
+              ))
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={() => handleRemove(user)} colorScheme="red">
+              Leave Group
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+
+
+export default UpdateGroupChatModal;
