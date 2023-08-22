eaa0ce162465d3099eb2cf7554596c104a46a4e9
vigkrishna <kvig1_be22@thapar.edu>
2023-08-22 20:14:31 +0530

    updated chatpage and added reportwebvitals js file

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -9,42 +9,42 @@ import "./chat.css";
 
 
  const ChatPage = () => {
-  // const [fetchAgain, setFetchAgain] = useState(false);
-  // const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
 
   
   return (
 
 
-    // <div style={{ width: "100%" }}>
-    //   {user && <SideDrawer />}
-    //   <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
-    //     {user && <MyChats fetchAgain={fetchAgain} />}
-    //     {user && (
-    //       <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-    //     )}
-    //   </Box>
-    // </div>
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
    
-    <>
+//     <>
 
-    <div className="conT">
+//     <div className="conT">
 
-    <div className="mainHead"> Welcome to InTouch
-</div>
+//     <div className="mainHead"> Welcome to InTouch
+// </div>
    
-    <Flex color='white'>
-  <Box w='30vw' p='4' bg='red.800'>
-    Box 1
-  </Box>
-  <Spacer />
-  <Box w='50vw' p='4' bg='green.700'>
-    Box 2
-  </Box>
-</Flex>
+//     <Flex color='white'>
+//   <Box w='30vw' p='4' bg='red.800'>
+//     Box 1
+//   </Box>
+//   <Spacer />
+//   <Box w='50vw' p='4' bg='green.700'>
+//     Box 2
+//   </Box>
+// </Flex>
     
-</div>
-    </>
+// </div>
+//     </>
     
     
   );
