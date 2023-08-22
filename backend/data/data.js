ff6c486ed0469f4d1f4641849f7be8952333ae9c
vigkrishna <kvig1_be22@thapar.edu>
2023-08-22 20:03:45 +0530

    dummy data added

---
+++ backend/data/data.js
@@ -0,0 +1,46 @@
+const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "John Doe",
+          email: "john@example.com",
+        },
+        {
+          name: "bnda",
+          email: "bnda@example.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName: "John Doe",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Guest User",
+          email: "guest@example.com",
+        },
+        {
+          name: "bnda",
+          email: "bnda@example.com",
+        },
+      ],
+      _id: "617a077e18c25468b27c4dd4",
+      chatName: "Guest User",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Anthony",
+          email: "anthony@example.com",
+        },
+        {
+          name: "bnda",
+          email: "bnda@example.com",
+        },
+      ],
+      _id: "617a077e18c2d468bc7c4dd4",
+      chatName: "Anthony",
+    }]
